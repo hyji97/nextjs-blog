3229758b931af57e040cb2d665f71c276c98e169
hyji97 <82359067+hyji97@users.noreply.github.com>
2021-04-12 21:23:15 +0900

    Initial commit

---
+++ pages/posts/first-post.js
@@ -0,0 +1,26 @@
+import Link from 'next/link'
+export default function FirstPost(){
+    return (
+    <>
+    <h1>First Post
+    </h1>
+    <h2>
+    <Link href="/">
+        <a>Back to home</a>
+    </Link>
+    </h2>
+    <h1>Gamzaking</h1>
+    <h2>Gamzaking is the king of gamza</h2>
+    <h1>
+    my name is Kimchi who loves the movie 'Kimchi warrior'
+    </h1>
+    <h1>
+    Some radishes are so hot to eat
+    </h1>
+    <h1>
+    I'd never know that '애프터서비스' is Konglish
+    </h1>
+    <h1>The Movie 'Finding for Neemo' is my most favorite one</h1>
+    </>
+)
+}
